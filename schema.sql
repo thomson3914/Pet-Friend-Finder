8e2bb63524bb4fdf99878ef561add9f0f81ac24a
thomson3914 <thomson3914@gmail.com>
2018-08-30 20:15:16 -0400

    initial commit

---
+++ schema.sql
@@ -0,0 +1,4 @@
+DROP DATABASE IF EXISTS friends_db;
+
+CREATE DATABASE friends_db;
+
--- server.js
+++ server.js
@@ -1,27 +1,43 @@
-// Dependencies 
-var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+// var methodOverride = require("method-override");
+var db = require("./models");
 
 
-// Creates an instance of "express server"
+// Sets up the Express App
+// =============================================================
 var app = express();
-var PORT = process.env.PORT || 3306;
+var PORT = process.env.PORT || 8080;
 
-//connection
-var connection = require('./config/connection.js')
+// app.get('/', function(req, res) {
+//     res.redirect('/signin');
+// });
 
-// Incorporate body-parser middleware
-app.use(bodyParser.json());                                     // parse application/json
-app.use(bodyParser.urlencoded({extended: true}));               // parse application/x-www-form-urlencoded
-app.use(bodyParser.text());                                     // allows bodyParser to look at raw text
-app.use(bodyParser.json({ type: 'application/vnd.api+json'}));  // parse application/vnd.api+json as json
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(express.static("public"));
+
+// // Sets up Express Engine Handlebars
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 // Routes
-require('./app/routing/api-routes.js')(app);
-require('./app/routing/html-routes.js')(app);
+// =============================================================
+var routes = require("./controllers/friend_controller.js");
+app.use("/", routes);
 
-// Listener
-app.listen(PORT, function() {
-	console.log('App listening on PORT ' + PORT);
-})
+
+// Starts the server to begin listening
+// =============================================================
+db.sequelize.sync({ force: true }).then(function() {
+    app.listen(PORT, function() {
+        console.log("App listening on PORT " + PORT);
+    });
+});
